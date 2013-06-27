70d696732fbac4210f0aeba565b1356bcd4c1d04
Carla Ares <carla.ares@gmail.com>
2013-06-27 19:05:28 -0300

    [#51161171] - Changes made in order to send triggers.

--- lib/exact_target_sdk/triggered_send.rb
+++ lib/exact_target_sdk/triggered_send.rb
@@ -3,6 +3,7 @@ class TriggeredSend < APIObject
 
   array_property 'Attributes'
   array_property 'Subscribers'
+  array_property 'Client'
   property 'TriggeredSendDefinition', :required => true
 
 end
